1e169c20acb2ee4c549561aeb2abad92f21b2f6c
bayu.tanmizi <bayu.tanmizi@adelphi.digital>
2017-05-09 15:46:52 +0800

    gulp copy set up

--- gulp/copy.js
+++ gulp/copy.js
@@ -6,8 +6,25 @@ export default function(gulp, plugins, args, config, taskTarget, browserSync) {
   let dirs = config.directories;
   let dest = path.join(taskTarget);
 
+  // Copy fonts
+  gulp.task('copy.fonts', () => {
+    return gulp.src([path.join(dirs.source, dirs.icons, '**/*'), path.join(dirs.source, dirs.fonts, '**/*')])
+      .pipe(gulp.dest(path.join(dest, '/assets/fonts')));
+  });
+
+  // Copy styles
+  gulp.task('copy.styles', () => {
+    return gulp.src([path.join(dirs.source, dirs.styles, '**/*.css')])
+      .pipe(gulp.dest(path.join(dest, '/assets/styles')));
+  });
+
+  // Copy api
+  gulp.task('copy.api', () => {
+    return gulp.src([path.join(dirs.source, dirs.api, '**/*')])
+      .pipe(gulp.dest(path.join(dest, '/assets/api')));
+  });
   // Copy
-  gulp.task('copy', () => {
+  gulp.task('copy', ['copy.fonts', 'copy.styles', 'copy.api'], () => {
     return gulp.src([
       path.join(dirs.source, '**/*'),
       '!' + path.join(dirs.source, '{**/\_*,**/\_*/**}'),
--- src/_scripts/main.js
+++ src/_scripts/main.js
@@ -6,7 +6,38 @@
 import $ from 'jquery';
 import Link from '../_modules/link/link';
 
+// import Header from '../_modules/organisms/standard-header/standard-header';
+// import Weather from '../_modules/organisms/weather/weather';
+// import PlanTrip from '../_modules/organisms/plan-trip/plan-trip';
+
 $(() => {
   new Link(); // Activate Link modules logic
   console.log('Welcome to Yeogurt!');
 });
+
+
+// $(() => {
+//   new Link(); // Activate Link modules logic
+//   new Header();
+//   new Search();
+//   if ($('.plan-trip-container').length) {
+//     new Weather();
+//     new PlanTrip();
+//     new ActivityCalendar();
+//   }
+//   // new SocialGallery();
+//   new StandardHeader();
+//   if ($('.newsroom-main-wrap').length) {
+//     new Newsroom();
+//   }
+//   if ($('.contactform-wrapper').length) {
+//     new ContactForm();
+//   }
+//   if ($('.onemap-container').length) {
+//     new Onemap();
+//   }
+//   if ($('.event-listing-main').length) {
+//     new EventsListing();
+//   }
+//   // console.log('Welcome to Yeogurt!');
+// });
